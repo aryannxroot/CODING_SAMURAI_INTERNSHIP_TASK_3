2c016b037b5ecbe4296e2fc8f8659abb2d47c1b5
Aryan <69583042+aryannxroot@users.noreply.github.com>
2024-06-19 14:47:11 +0530

    Done with the API calls

---
+++ client/src/constants/config.js
@@ -0,0 +1,31 @@
+//API_NOTIFICATION_MESSAGES
+export const API_NOTIFICATION_MESSAGES = {
+    loading : {
+        title : 'Loading...',
+        msg : 'Data is being loaded...'
+    },
+    success : {
+        title : 'Success',
+        msg : 'Data loaded successfully'
+    },
+    responseFailure: {
+        title:'Error',
+        msg : 'An error occured while fetching data. Please try again later'
+    },
+    requestFailure: {
+        title:'Error',
+        msg : 'An error occured while parsing request data'
+    },
+    networkError:{
+        title : 'Network Error',
+        msg : 'Please check your internet connection'
+    }
+}
+
+//API SERVICE CALL
+//SAMPLE REQUEST
+//NEED SERVICE CALL: { url : '/', method : 'POST/GET/DELETE/PUT' , params : true/false, query : true/false  }
+
+export const SERVICE_URLS = {
+    userSignUp:{ url : '/signup', method : 'POST'}
+}
